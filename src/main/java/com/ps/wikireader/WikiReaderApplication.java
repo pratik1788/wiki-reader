b8af97eaaee6f64f86731a20de68e25da9d21502
pratiksurti <pratik.surti17@gmail.com>
2021-08-27 21:45:08 -0400

    Initial commit

---
+++ src/main/java/com/ps/wikireader/WikiReaderApplication.java
@@ -0,0 +1,13 @@
+package com.ps.wikireader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ps/wikireader/WikiReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.wikireader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
